03d53e22a654a7012b1565aaa9fd0e96eafc856b
lomen_ha <the9616@lomen.kr>
2022-08-15 15:07:32 +0900

    list detail

--- components/Seo.jsx
+++
@@ -1,3 +0,0 @@
-export default function Seo({ props }) {
-  return <div className="mx-auto w-full border-b py-2 text-black">{props}</div>;
-}
---
+++ pages/detail/[id].jsx
@@ -0,0 +1,76 @@
+import Layout from 'components/Layout';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+export default function Detail() {
+  const router = useRouter();
+  return (
+    <Layout deps>
+      <div className="mt-10 space-y-6">
+        <div className="aspect-video w-full bg-gray-300"></div>
+        <div className="flex items-center space-x-4 border-b pb-4">
+          <div className="aspect-square h-10 rounded-full bg-gray-300"></div>
+          <div className="">
+            <p className="text-sm">Steve Jeds</p>
+            <Link href="#">
+              <a className="text-sm text-gray-400">View profile →</a>
+            </Link>
+          </div>
+        </div>
+        <div className="space-y-2">
+          <h1 className="text-2xl font-semibold">iPhone14</h1>
+          <p className="text-lg">$140</p>
+          <p className="font-light">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id
+            eligendi sapiente, quos eos explicabo quisquam nisi culpa
+            consequuntur rerum asperiores voluptate facere facilis atque?
+            Laboriosam veniam dolore nemo odio dicta!
+          </p>
+        </div>
+        <div className="flex justify-between gap-x-2 text-center">
+          <button className="flex-1 rounded-lg bg-orange-400 py-2 text-white">
+            Talk to seller
+          </button>
+          <button className="hover:text-orange-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="space-y-4">
+          <h1 className="text-2xl font-semibold">Similar items</h1>
+          {new Array(3).fill(0).map((_, idx) => (
+            <>
+              <div key={idx} className="mb-4 flex gap-x-4">
+                <Link href="#">
+                  <a className="flex-1">
+                    <div className="mb-2 aspect-square w-full bg-gray-300"></div>
+                    <p>iPhone14</p>
+                    <p>$140</p>
+                  </a>
+                </Link>
+                <Link href="#">
+                  <a className="flex-1">
+                    <div className="mb-2 aspect-square w-full bg-gray-300"></div>
+                    <p>iPhone14</p>
+                    <p>$140</p>
+                  </a>
+                </Link>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+}
